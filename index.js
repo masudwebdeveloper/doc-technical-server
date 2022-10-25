3c949c4def1cdb0255a87a01a6a912fc3a915e5d
masudrana5 <abdullainfo30@gmail.com>
2022-10-25 15:14:01 +0600

    courses details server ok

--- index.js
+++ index.js
@@ -4,10 +4,38 @@ const cors = require('cors')
 
 const port = process.env.port || 5000
 
-app.get(cors())
+const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
    res.send('doc-technical server is running')
+})
+
+app.get('/categories-courses', (req, res) => {
+   res.send(categories)
+})
+
+app.get('/courses', (req, res) => {
+   res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+   const id = req.params.id;
+   const singleCourse = courses.find(course => course._id === id);
+   res.send(singleCourse);
+})
+
+app.get('/course-category/:id', (req, res) => {
+   const id = req.params.id;
+   if (id === "07") {
+      res.send(courses)
+   } else {
+
+      const courseDetails = courses.filter(course => course.category_id === id);
+      res.send(courseDetails);
+   }
 })
 
 app.listen(port, () => {
